809b329d5a48fd72eea078b2b6c04b7ea6f6a08e
Cindy Wang <ciwang@stanford.edu>
2016-11-17 00:42:09 -0800

    finished up features: readability, spelling errors, counts

--- feature_generator.py
+++ feature_generator.py
@@ -2,9 +2,21 @@ from fg_util import *
 
 # Given a set of comments, returns a feature vector representing those comments
 def extract_features(self, comment):
-	result = {}
 
 	stats = TextStats(comment)
-	entity_names = extract_entity_names(stats[])
+	logWordCount = stats.log_word_count()
+	maxWordLen = stats.max_word_len()
+	readability = stats.SMOG_readability()
+	numSpellingErrors = stats.num_spelling_errors()
+	entityNames = stats.extract_entity_names()
+	
+	result = {
+		'log_word_count': logWordCount,
+		'max_word_len': maxWordLen,
+		'readability': readability, # might have to take the log of this
+		'num_spelling_errs': numSpellingErrors
+	}
+	for name in entityNames:
+		result[('entity', name)] = 1
 
 	return result
