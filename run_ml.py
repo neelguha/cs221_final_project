140d3bb1cad865c926620d764483a11f09dd308f
Neel Guha <nguha@stanford.edu>
2016-11-17 19:05:32 -0800

    Fixed the way we test the classifier. Also added a dummy feature return for debugging. Will try to speed up feature extraction in next commit

--- run_ml.py
+++ run_ml.py
@@ -3,31 +3,70 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 import random
 from feature_generator import *
+from sklearn.metrics import precision_recall_fscore_support
 
-
-USER_COMMENT_SUBSETS = 100 # How many distinct sets we want to split a single user's comments into 
+USER_COMMENT_SUBSETS = 10 # How many distinct sets we want to split a single user's comments into 
 COMMENT_WORD_THRESHOLD = 50 # Ignore comments with fewer than this many words
 USERS_TO_TRACK = 20 # Number of users to build classifier for 
 
-
-all_users = defaultdict(list)
+user_dict = defaultdict(list)
+all_comments = []
+all_users  = []
 for line in open("sampled_users.tsv"):
 	if line[0] == "#": continue
 	author_id,comment_id,timestamp,text = line.strip().split("\t")
 	comment_dict = {'author_id':author_id,'comment_id':comment_id,'timestamp':timestamp,'text':text}
-	all_users[author_id].append(comment_dict)	
+	user_dict[author_id].append(comment_dict)
+	all_users.append(author_id)	
+	all_comments.append(text)
+
+users_to_test = np.random.choice(user_dict.keys(),USERS_TO_TRACK,replace = False)
+print users_to_test
+for user in users_to_test:
+	user_features = []
+	user_comments = np.random.choice(user_dict[user],40) 
+	for comment in user_comments:
+		user_features.append(extract_features(comment))
+	other_comments = np.random.choice(all_comments,40) 
+	other_features = []
+	for comment in other_comments:
+		other_features.append(extract_features(comment))
+	print len(user_features),len(other_features)
+	user_train,user_test = np.array_split(user_features,2)
+	other_train,other_test = np.array_split(other_features,2)
+	print len(other_train),len(user_train)
+	all_train = list(user_train) + list(other_train)
+	train_vals = [True]*len(user_train) + [False]*len(other_train)
+	logreg = LogisticRegression()
+	logreg.fit(all_train,train_vals)
+
+	all_test = list(user_test) + list(other_test)
+	test_vals = [True]*len(user_test) + [False]*len(other_test)
+	predictions = logreg.predict(all_test)
+	print precision_recall_fscore_support(test_vals, predictions, average='binary')
+
+
 
 # Filter comments which don't meet length threshold and split comments per user into subset
-filtered_comments = {}
+'''filtered_comments = {}
 for user in all_users:
 	user_comments = []
 	for comment in all_users[user]:
-		if len(comment['text']) > COMMENT_WORD_THRESHOLD:
+		if len(comment['text'].split(" ")) > COMMENT_WORD_THRESHOLD:
 			user_comments.append(comment['text'])
 	random.shuffle(user_comments)
 	if len(user_comments) == 0:
 		continue
-	filtered_comments[user] = np.array_split(user_comments,USER_COMMENT_SUBSETS)
+	comments = np.array_split(user_comments,USER_COMMENT_SUBSETS)
+	features = [extract_features(comment) for comment in comments]
+	filtered_comments[user] = features
+
+
+
+
+
+
+
 
 # pick sample of users to build a classifier for 
 users_to_track = np.random.choice(filtered_comments.keys(),USERS_TO_TRACK,replace = False)
@@ -41,37 +80,23 @@ for track_user in users_to_track:
 	test_comment_features = []
 	test_values = []
 	for user in filtered_comments:
-		comments = filtered_comments[user]
+		comment_features = filtered_comments[user]
 		train_indices = np.random.choice(range(len(comments)),USER_COMMENT_SUBSETS / 2,replace = False)
-		for i in range(len(comments)):
+		for i in range(len(comment_features)):
 			if i in train_indices:
-				train_comment_features.append(extract_features(comments[i]))
-				train_keys.update(d.keys())
+				train_comment_features.append(comment_features[i])
 				train_values.append(user == track_user)
-			else: 
-				test_comment_features.append(extract_features(comments[i]))
-				test_keys.update(d.keys())
+			else:
+				test_comment_features.append(comment_features[i])
 				test_values.append(user == track_user)
-	
-	#convert train_comment_features and test_comment_features from list of dicts to list of lists
-	train_features = []
-	test_features = []
-	for d in train_comment_features:
-		train_features.append({key: d.get(key, 0) for key in train_keys})
-	for d in test_comment_features:
-		test_features.append({key: d.get(key, 0) for key in test_keys})
-
+	print len(test_values), sum(test_values)
 	logreg = LogisticRegression()
-	#print len(train_comment_features)
-	print sum(test_values)
 	logreg.fit(train_comment_features,train_values)
 	predictions = logreg.predict(test_comment_features)
-	num_correct = 0
-	#print predictions
-	for i in range(len(predictions)):
-		if predictions[i] == test_values[i]:
-			num_correct += 1
-	print num_correct,len(predictions)
+	#test_values = [1,1,1,0,0,0]
+	#predictions = [1,1,0,0,0,0]
+	print precision_recall_fscore_support(test_values, predictions, average='binary')'''
+	
 
 
 
