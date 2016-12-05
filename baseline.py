5cc270a3fad4c9f5ddd9764ef512d855493fe71b
Annie Hu <anniehu@stanford.edu>
2016-12-04 21:27:30 -0800

    added baseline and oracle

--- baseline.py
+++ baseline.py
@@ -30,8 +30,7 @@ for line in open("sampled_users.tsv"):
 	index += 1
 
 users_to_test = np.random.choice(user_dict.keys(),USERS_TO_TRACK,replace = False)
-output_file = open("single_user_results.csv","w")
-tfidf = tfidf_util.TFIDF(comment['text'] for comment in all_comments)
+output_file = open("single_user_results_baseline.csv","w")
 
 precisionScore = 0.0
 recallScore = 0.0
@@ -41,24 +40,21 @@ for user in users_to_test:
 	user_features = []
 	user_comments = np.random.choice(user_dict[user],USER_TRAIN_TEST_SIZE) 
 	for comment in user_comments:
-		user_features.append(extract_features(comment, tfidf))
+		user_features.append([len(comment['text'].split(" "))])
 	other_comments = np.random.choice([comment for comment in all_comments if comment['author_id'] != user],OTHER_TRAIN_TEST_SIZE) 
 	other_features = []
 	for comment in other_comments:
-		other_features.append(extract_features(comment, tfidf))
+		other_features.append([len(comment['text'].split(" "))])
 	user_train,user_test = np.array_split(user_features,2)
 	other_train,other_test = np.array_split(other_features,2)
 
 	logreg = LogisticRegression()
-	dv = DictVectorizer(sparse=True)
 
 	all_train = list(user_train) + list(other_train)
-	all_train = dv.fit_transform(all_train)
 	train_vals = [True]*len(user_train) + [False]*len(other_train)
 	logreg.fit(all_train,train_vals)
 
 	all_test = list(user_test) + list(other_test)
-	all_test = dv.transform(all_test) #ONLY DO TRANSFORM AND NOT FIT FOR TEST VALS
 	test_vals = [True]*len(user_test) + [False]*len(other_test)
 	predictions = logreg.predict(all_test)
 	precision,recall,fbeta_score,support = precision_recall_fscore_support(test_vals, predictions, average='binary')
@@ -71,4 +67,3 @@ for user in users_to_test:
 if verbose:
 	print "Avg Precision:%f\n\tAvg Recall:%f" % (precisionScore/len(users_to_test), recallScore/len(users_to_test))
 output_file.write("Avg:%f,%f" % (precisionScore/len(users_to_test), recallScore/len(users_to_test)))
-
