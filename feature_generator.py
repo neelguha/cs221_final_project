f614e9741ae4fea2179a0798844ab385363e5908
Cindy Wang <ciwang@stanford.edu>
2016-12-04 17:42:51 -0800

    used tfidf for entity extraction

--- feature_generator.py
+++ feature_generator.py
@@ -3,19 +3,21 @@ import random
 import tfidf_util
 # Given a set of comments, returns a feature vector representing those comments
 
-def extract_tfidf(comment, tfidf):
-	tfidfScore = tfidf.entities(comment)
+# def extract_tfidf(comment, tfidf):
+# 	tfidfScore = tfidf.entities(comment)
 
 def extract_features(comment, tfidf):
-	stats = TextStats(comment)
+	stats = TextStats(comment['text'])
 	logWordCount = stats.log_word_count()
 	maxWordLen = stats.max_word_len()
 	readability = stats.SMOG_readability()
 	numSpellingErrors = stats.num_spelling_errors()
-	entityNames = stats.extract_entity_names()
+	#entityNames = stats.extract_entity_names()
 	punctScore = stats.punctuation_score()
 	profanityScore = stats.profanity_score()
 	#sentimentScore = stats.sentiment_score()
+	entityNames = tfidf.entities(comment['index'])
+	print entityNames
 	
 	result = {
 		'log_word_count': logWordCount,
--- tfidf_util.py
+++ tfidf_util.py
@@ -1,16 +1,20 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 
 class TFIDF:
 	def __init__(self, comments):
 		self.tf = TfidfVectorizer(input='content', analyzer='word', min_df = 1, stop_words = 'english', ngram_range = (1,2))
 		self.matrix = self.tf.fit_transform(comments)
+		self.features = self.tf.get_feature_names()
+		self.threshold = 0.2
 
-	def entities(self, comment):
-		# for i in range(len(comment) - 1): # unigrams and bigrams
+	def entities(self, comment_index):
+		entities = []
 
-		# get last word
-		names = self.tf.get_feature_names()
-		values = dict(zip(names, self.matrix.data))
-		print values
-		return values
+		comment_features = self.matrix[comment_index,:].nonzero()[1]
+		for cf in comment_features:
+			if self.matrix[comment_index, cf] > self.threshold:
+				entities.append(self.features[cf])
+
+		return entities
