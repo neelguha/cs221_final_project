24473b0375b21b1fcb965828fceb20a43cc30d20
Cindy Wang <ciwang@stanford.edu>
2016-12-04 17:42:51 -0800

    used tfidf for entity extraction

--- single_user_classification.py
+++ single_user_classification.py
@@ -4,11 +4,12 @@
 
 from collections import defaultdict
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 import random
-from feature_generator import *
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
 import tfidf_util
+from feature_generator import *
 
 USERS_TO_TRACK = 20 # Number of users to build classifier for
 USER_TRAIN_TEST_SIZE = 40 # Number of target user's comments to train 
@@ -18,40 +19,45 @@ verbose = True
 user_dict = defaultdict(list)
 all_comments = []
 all_users  = []
+index = 0
 for line in open("sampled_users.tsv"):
 	if line[0] == "#": continue
 	author_id,comment_id,timestamp,text = line.strip().split("\t")
-	comment_dict = {'author_id':author_id,'comment_id':comment_id,'timestamp':timestamp,'text':text}
+	comment_dict = {'author_id':author_id,'index':index,'timestamp':timestamp,'text':text}
 	user_dict[author_id].append(comment_dict)
 	all_users.append(author_id)	
-	all_comments.append(text)
+	all_comments.append(comment_dict)
+	index += 1
 
 users_to_test = np.random.choice(user_dict.keys(),USERS_TO_TRACK,replace = False)
 output_file = open("single_user_results.csv","w")
-tfidf = tfidf_util.TFIDF(all_comments)
+tfidf = tfidf_util.TFIDF(comment['text'] for comment in all_comments)
 
 for user in users_to_test:
 	if verbose: print "User:",user
 	user_features = []
 	user_comments = np.random.choice(user_dict[user],USER_TRAIN_TEST_SIZE) 
 	for comment in user_comments:
-		user_features.append(extract_tfidf(comment, tfidf))
-		user_features.append(extract_features(comment['text']))
-	other_comments = np.random.choice(all_comments,OTHER_TRAIN_TEST_SIZE) 
+		#user_features.append(extract_tfidf(comment, tfidf))
+		user_features.append(extract_features(comment, tfidf))
+	other_comments = np.random.choice([comment for comment in all_comments if comment['author_id'] != user],OTHER_TRAIN_TEST_SIZE) 
 	other_features = []
 	for comment in other_comments:
-		other_features.append(extract_tfidf(comment, tfidf))
-		other_features.append(extract_features(comment))
+		#other_features.append(extract_tfidf(comment, tfidf))
+		other_features.append(extract_features(comment, tfidf))
 	user_train,user_test = np.array_split(user_features,2)
 	other_train,other_test = np.array_split(other_features,2)
-	all_train = list(user_train) + list(other_train)
-	train_vals = [True]*len(user_train) + [False]*len(other_train)
-
 
 	logreg = LogisticRegression()
+	dv = DictVectorizer(sparse=True)
+
+	all_train = list(user_train) + list(other_train)
+	all_train = dv.fit_transform(all_train)
+	train_vals = [True]*len(user_train) + [False]*len(other_train)
 	logreg.fit(all_train,train_vals)
 
 	all_test = list(user_test) + list(other_test)
+	all_test = dv.transform(all_test) #ONLY DO TRANSFORM AND NOT FIT FOR TEST VALS
 	test_vals = [True]*len(user_test) + [False]*len(other_test)
 	predictions = logreg.predict(all_test)
 	precision,recall,fbeta_score,support = precision_recall_fscore_support(test_vals, predictions, average='binary')
