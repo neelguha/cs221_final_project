df83b22ee12f1a10c8cefb9e5cda30729e9e470c
Neel Guha <nguha@stanford.edu>
2016-12-15 01:04:35 -0800

    adding all files

--- multi_user_classification.py
+++ multi_user_classification.py
@@ -8,12 +8,16 @@ from sklearn.linear_model import LogisticRegression
 import random
 from feature_generator import *
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn import svm
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.naive_bayes import GaussianNB
+
 
 verbose = True
 
 user_dict = defaultdict(list)
 for line in open("sampled_users_features.tsv"):
-	user,comment_id,timestamp,text,feature_text = line.strip().split("\t")
+	user,feature_text = line.strip().split("\t")
 	feature_vector = feature_text.strip().split(",")
 	feature_vector = [float(x) for x in feature_vector if len(x) > 0]
 	user_dict[user].append(feature_vector)
@@ -22,9 +26,10 @@ train_X = []
 train_Y = []
 test_X = []
 test_Y = []
-i = 0
-for user in user_dict:
-	i += 1
+#i = len(user_dict.keys())
+i = 10
+users_to_test = np.random.choice(user_dict.keys(),i)
+for user in users_to_test:
 	num_comments = len(user_dict[user])
 	features = user_dict[user]
 	random.shuffle(features)
@@ -35,20 +40,25 @@ for user in user_dict:
 	for x in test:
 		test_X.append(x)
 		test_Y.append(user)
-	if i == 10:
-		break
 
 print "Number of training examples:",len(train_X)
 print "Number of test examples:",len(test_X)
 
-logreg = LogisticRegression()
-logreg.fit(train_X,train_Y)
-predictions = logreg.predict(test_X)
+#logreg = LogisticRegression()
+#logreg.fit(train_X,train_Y)
+#predictions = logreg.predict(test_X)
+#clf = svm.SVC(decision_function_shape='ovo')
+#clf.fit(train_X, train_Y)
+#predictions = clf.predict(test_X)
+gnb = GaussianNB()
+gnb.fit(train_X,train_Y)
+predictions = gnb.predict(test_X)
+
 micro_precision,micro_recall,micro_fbeta_score,micro_support = precision_recall_fscore_support(test_Y, predictions, average='micro')
 if verbose:
-	print "\tPrecision:%f\n\tRecall:%f" % (micro_precision,micro_recall)
+	print "Micro Precision:%f\nMicro Recall:%f" % (micro_precision,micro_recall)
 macro_precision,macro_recall,macro_fbeta_score,macro_support = precision_recall_fscore_support(test_Y, predictions, average='macro')
 if verbose:
-	print "\tPrecision:%f\n\tRecall:%f" % (macro_precision,macro_recall)
+	print "Macro Precision:%f\nMacro Recall:%f" % (macro_precision,macro_recall)
 output_file = open("multi_user_results.csv","w")
 output_file.write("%f,%f,%f,%f\n" %(micro_precision,micro_recall,macro_precision,macro_recall))
