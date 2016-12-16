d7bec785cc353d508bb8e73611d305882eb9e2f1
Neel Guha <nguha@stanford.edu>
2016-12-16 15:35:31 -0800

    Writing out README, reorganizing/cleaning

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-# cs221_final_project
+# Predicting Authorship of Reddit Comments 
+
+## Annie Hu, Cindy Wang, Neel Guha 
+
+
+This directory contains our scripts and data for this project. Below is a description of how to run the experiments. Because we took samples, replicating the entire process may lead to results that vary slightly from those described in the paper.
+
+
+
+### Data Pre-processing
+
+Commands for data processing/feature generation.
+
+1. python create_sample.py 
+
+Samples a set of users and a set of comments from those users from the file data/politics.tsv. Writes the results out to sampled_users.tsv
+
+2. python feature_generator.py 
+
+For each comment in our sample, generate the corresponding feature vector and write out the feature vector to data/sampled_users.tsv.
+
+
+### Binary Classification 
+
+1. python single_user_classification_precomputed_features.py
+
+Runs binary classification on all users and reports the average precision/recall. Adjusting flags in the script control which model (Naive Bayes, SVM, etc) is used.  
+
+
+### Multiclass Classification 
+
+
+
+### Unsupervised Methods
+
+
+
+
+
+
+
+
+
+Data Files 
+
+1. data/politics.tsv 
+Contains all comments on /r/politics from 2014. Data is in the form of
+
+<subreddit_name>  <time_stamp>  <subreddit_id>  <comment_id>  <parent_comment_id> <author_name> <score> <???> <thread/link_id> <text>
+
+
+
+
+
+
+
+
+
 
 Scripts 
 sample\_statistics.py: Reads in sampled\_users.tsv and outputs statistics about the data set (number of users, average number of comments, etc)
--- write_out_features.py
+++ write_out_features.py
@@ -12,7 +12,7 @@ def write_out_comment_features():
 	all_features = []
 	v = DictVectorizer(sparse=False)
 	i = 0
-	for line in open("sampled_users.tsv"):
+	for line in open("data/sampled_users.tsv"):
 		i += 1
 		if line[0] == "#": continue
 		author_id,comment_id,timestamp,text = line.strip().split("\t")
@@ -22,7 +22,7 @@ def write_out_comment_features():
 		if i % 100 == 0:
 			print i
 	X = v.fit_transform(all_features)
-	output_file = open("sampled_users_features.tsv","w")
+	output_file = open("data/sampled_users_features.tsv","w")
 	for i in range(len(X)):
 		output_file.write("%s," % all_users[i])
 		for var in X[i]:
@@ -30,40 +30,6 @@ def write_out_comment_features():
 		output_file.write("\n")
 	print v.get_feature_names()
 
-# Takes a user's comments, splits them into two sets and writes out a feature vector for each set. 
-def write_out_user_split_features():
-	output_file = open("sampled_users_split_features.tsv","w")
-	user_dict = defaultdict(list)
-	all_comments = []
-	all_users  = []
-	for line in open("sampled_users.tsv"):
-		if line[0] == "#": continue
-		author_id,comment_id,timestamp,text = line.strip().split("\t")
-		comment_dict = {'author_id':author_id,'comment_id':comment_id,'timestamp':timestamp,'text':text}
-		user_dict[author_id].append(comment_dict)
-		all_users.append(author_id)	
-		all_comments.append(text)
-	for user in user_dict:
-		user_comments = np.random.choice(user_dict[user],len(user_dict[user])) 
-		train_comments,test_comments = np.array_split(user_comments,2)
-		combined_comment = ""
-		for comment in train_comments:
-			combined_comment = combined_comment + " " + comment['text']
-		train_features = extract_features(combined_comment)
-		output_file.write(user + "\t")
-		for var in train_features:
-			output_file.write("%f," % var)
-		output_file.write("\n")
-
-		combined_comment = ""
-		for comment in test_comments:
-			combined_comment += " " + comment['text']
-		test_features = extract_features(combined_comment)
-		output_file.write(user + "\t")
-		for var in test_features:
-			output_file.write("%f," % var)
-		output_file.write("\n")
-
 write_out_comment_features()
-#write_out_user_split_features()
+
 		
